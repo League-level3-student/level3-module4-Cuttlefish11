3f9b654dd59f5844c5a1683b89cbb5dc6dd9fffc
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2023-08-07 18:25:18 -0700

    Smiles

--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -9,19 +9,29 @@ import javax.swing.JPanel;
 public class _02_TextUndoRedo {
     
      // Create a JFrame with a JPanel and a JLabel.
- JFrame frame = new JFrame();
+JFrame frame = new JFrame();
  JPanel panel = new JPanel();
  JLabel label = new JLabel();
+
+ 
      // Every time a key is pressed, add that character to the JLabel. It should
       //look like a basic text editor.
      public static void main(String[] args) {
 		
 	}
+     public void blarg() {
+    	 frame.add(panel);
+    	 panel.add(label);
+    	 frame.pack();
+    	 frame.setVisible(true);
+     }
      public void keyPressed(KeyEvent e) {
     char c = e.getKeyChar();
     //if(c == 'a' ||c == 'b' || c == 'c' ||c == 'd' ||c == 'e' ||c == 'f' ||c == 'g' ||c == 'h' ||c == 'i' ||c == 'j' ||c == 'k' ||c == 'l' ||c == 'm' ||c == 'n' ||c == 'o' ||c == 'p' ||c == 'q' ||c == 'r' ||c == 's' ||c == 't'||c == 'u' ||c == 'v' ||c == 'w' ||c == 'x' ||c == 'y' ||c == 'z') {
     //}
-    
+    if(c>='a' && c<='z' || c>='A' && c<='Z') {
+    	
+    }
  	}
      // Make it so that every time the BACKSPACE key is pressed, the last
      // character is erased from the JLabel.
